ed2abfd88a7625c32ccdde22a3b8ef9ad9ee008e
Sudhakiran Kallagunta <sudhakiran@yellowblock.net>
2024-05-08 15:25:51 +0530

    for email

--- Status_HyperpayFunction/GasStation.cs
+++ Status_HyperpayFunction/GasStation.cs
@@ -1,154 +1,80 @@
-using System;
-using System.Data.SqlClient;
-using System.Data;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Status_HyperpayFunction.Utils;
-using Status_HyperpayFunction.ViewModels;
 using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using RestSharp.Authenticators;
+using Newtonsoft.Json.Linq;
 using RestSharp;
-using System.Net;
+using System;
 
 namespace Status_HyperpayFunction
 {
     public class GasStation
     {
         [FunctionName("GasStation")]
-        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("* * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            CompanyConfiguration(log);
+            GetGasBalance(log);
         }
 
-        private async void CompanyConfiguration(ILogger log)
+        private void GetGasBalance(ILogger log)
         {
-            CompanyConfigration companyConfigration = new CompanyConfigration();
-            string query = "select Name,ValueorJson from Common.TenantConfigurations where Name = 'CompanyConfiguration'";
-            SqlConnection sqlConnection = new SqlConnection(CommonConnection.DBConnection);
-
-            SqlCommand sqlCommand = new(query, sqlConnection);
-            sqlConnection.Open();
-            sqlCommand.CommandType = CommandType.Text;
-            SqlDataReader dr = sqlCommand.ExecuteReader();
-            string name = null;
-            string valueorJson = null;
-            if (dr.HasRows)
-            {
-                while (dr.Read())
-                {
-                    name = dr["Name"].ToString();
-                    valueorJson = dr["ValueorJson"].ToString();
-                }
-            }
-            sqlConnection.Close();
-            List<CompanyConfigration> admimgasamount = JsonConvert.DeserializeObject<List<CompanyConfigration>>(name);
-            var gasStation = admimgasamount.FirstOrDefault(a => a.AddressType.ToLower() == "gasStation" && a.NetworkId.ToUpper() == "TRX_TEST");
-            decimal maxFeePerGasLimit = 300.0M;
             var fbEstimatedFee = GetEstimatedGasFee(log);
-            if (fbEstimatedFee.maxFeePerGas < maxFeePerGasLimit)
-            {
-                await SendEmail("exchangapayadmin@gmail.com", "Transaction at the gas station has been completed. Please fill the gas", "");
-            }
         }
 
 
-        private static EstimateGas GetEstimatedGasFee(ILogger log)
+        private static Root GetEstimatedGasFee(ILogger log)
         {
             log.LogInformation($"GetEstimatedGasFee Method Started at: {DateTime.Now}");
             var token = "Bearer ";
-            var client = new RestClient("" + "Fireblocks/FireBlockGasEstimateFee?assetId=trx");
+            var client = new RestClient("http://integration.southeastasia.cloudapp.azure.com/kraken-tst" + "/Fireblocks/GetAccountBalance?vaultAccountId=5628&wallet=TRX_TEST");
             var request = new RestRequest(Method.GET);
             request.AddHeader("authorization", token);
             IRestResponse response = client.Execute(request);
-            var fbEstimatedFee = JsonConvert.DeserializeObject<EstimateGas>(response.Content);
-            log.LogInformation($"GetEstimatedGasFee Method Responce is: {response.Content}");
+
+
+            JObject jsonObject = JObject.Parse(response.Content);
+
+            var fbEstimatedFee = JsonConvert.DeserializeObject<Root>(jsonObject.ToString());
+
+            //JObject dada = JObject.Parse(response.Content);
+
+            //var fbEstimatedFee = JsonConvert.DeserializeObject<Root>(dada.ToString());
+            //log.LogInformation($"GetEstimatedGasFee Method Responce is: {response.Content}");
             return fbEstimatedFee;
         }
-
-
-        private async Task SendEmail(string toEmail, string subject, string template)
+        public class Root
         {
-            Email emailClass = new Email();
-            emailClass.IsHtml = true;
-            emailClass.FromAddress = null;
-            emailClass.ToAddress = toEmail;
-            emailClass.Subject = subject;
-            emailClass.Body = template;
-            var isMailSend = SendMailgun(emailClass);
+            [JsonProperty("id")]
+            public string Id { get; set; }
+
+            [JsonProperty("total")]
+            public string Total { get; set; }
+
+            [JsonProperty("balance")]
+            public string Balance { get; set; }
+
+            [JsonProperty("lockedAmount")]
+            public string LockedAmount { get; set; }
+
+            [JsonProperty("available")]
+            public string Available { get; set; }
+
+            [JsonProperty("pending")]
+            public string Pending { get; set; }
+
+            [JsonProperty("frozen")]
+            public string Frozen { get; set; }
+
+            [JsonProperty("staked")]
+            public string Staked { get; set; }
+
+            [JsonProperty("blockHeight")]
+            public string BlockHeight { get; set; }
+
+            [JsonProperty("blockHash")]
+            public string BlockHash { get; set; }
         }
-        private object SendMailgun(Email email)
-        {
-            SqlConnection sqlConnection = new SqlConnection(CommonConnection.DBConnection);
-            string query = "select APIKey,BaseUri,Domain,FromMail from [Configuration].[MailConfiguration] where Type = 'Mailgun'";
-            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
-            sqlConnection.Open();
 
-            try
-            {
-                sqlCommand.CommandType = CommandType.Text;
-                SqlDataReader dr = sqlCommand.ExecuteReader();
-                if (dr.HasRows)
-                {
-                    while (dr.Read())
-                    {
-                        email.BaseUri = dr["BaseUri"].ToString();
-                        email.ApiKey = dr["APIKey"].ToString();
-                        email.Domain = dr["Domain"].ToString();
-                        email.FromAddress = dr["FromMail"].ToString();
-                    }
-                }
-                sqlConnection.Close();
-            }
-            catch (Exception)
-            {
-                sqlConnection.Close();
-                return false;
-            }
-
-            RestClient client = new RestClient();
-            client.BaseUrl = new Uri(email.BaseUri);
-            client.Authenticator = new HttpBasicAuthenticator("api", email.ApiKey);
-            RestRequest request = new RestRequest();
-            request.AddParameter("domain", email.Domain, ParameterType.UrlSegment);
-            request.Resource = "/messages";
-            request.AddParameter("from", email.FromAddress);
-            request.AddParameter("to", email.ToAddress);
-            email.IsHtml = true;
-            if (!String.IsNullOrEmpty(email.BccAddress)) request.AddParameter("bcc", email.BccAddress);
-
-            request.AddParameter("subject", email.Subject);
-
-            if (email.IsHtml) request.AddParameter("html", email.Body);
-            else request.AddParameter("text", email.Body);
-
-            request.Method = Method.POST;
-            IRestResponse response1 = client.Execute(request);
-
-            if (response1.StatusCode == HttpStatusCode.OK)
-                email.Responce = true;
-            else
-                email.Responce = false;
-
-            return email.Responce;
-
-        }
-        public class EstimateGas
-        {
-            public decimal lowFeePerGas { get; set; }
-            public decimal medFeePerGas { get; set; }
-            public decimal highFeePerGas { get; set; }
-            public decimal maxFeePerGas { get; set; }
-            public decimal maxPriorityFeePerGas { get; set; }
-
-            public decimal GasFeeInEthLow { get; set; }
-            public decimal GasFeeInEthMed { get; set; }
-            public decimal GasFeeInEthHigh { get; set; }
-        }
     }
 }
 
