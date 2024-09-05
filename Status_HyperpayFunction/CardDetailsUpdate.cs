5b692054c92e1ccb23b8da18212a57f0ba46558e
Ajaykumar S <ajaykumar@yellowblock.net>
2024-09-05 09:32:54 +0530

    Commits changes for CardDetailsUpdate

---
+++ Status_HyperpayFunction/CardDetailsUpdate.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using Status_HyperpayFunction.Utils;
+
+namespace Status_HyperpayFunction
+{
+    public class CardDetailsUpdate
+    {
+        [FunctionName("CardDetailsUpdate")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            try
+            {
+                var client = new RestClient(CommonConnection.RestAPIURL + "api/v1/UpdateCardDetails");
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("content-type", "application/json");
+                var responce = client.Execute(request);
+                log.LogInformation($"responce Result is  (" + responce.Content + ")");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }
+}
